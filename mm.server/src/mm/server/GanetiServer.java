fc95299bdd3467ce089f8a4a79283dfa7e6fccac
Benedikt <benedikt-bakker@t-online.de>
2015-01-13 16:21:04 +0100

    Server Klasse

---
+++ mm.server/src/mm/server/GanetiServer.java
@@ -0,0 +1,12 @@
+package mm.server;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+public class GanetiServer {
+
+}
