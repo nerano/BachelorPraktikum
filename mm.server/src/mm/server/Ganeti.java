457558fbc9026111297652cadf52bb8048776fef
Benedikt <benedikt-bakker@t-online.de>
2015-01-15 11:39:53 +0100

    Ganeti Server

---
+++ mm.server/src/mm/server/Ganeti.java
@@ -0,0 +1,13 @@
+package mm.server;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("/ganeti")
+public class Ganeti {
+
+}
