5f9e552e2f731241d746421e8ab9c1841d3cfd70
julian <julian.harbarth@gmx.de>
2015-02-17 14:38:06 +0100

    power initialize + parser

--- mm.power/src/mm/power/main/PowerData.java
+++ mm.power/src/mm/power/main/PowerData.java
@@ -1,108 +1,100 @@
 package mm.power.main;
 
-import java.util.LinkedList;
+import java.util.HashMap;
 
 import mm.power.modeling.PowerSupply;
 
 public class PowerData {
 
+  private static HashMap<String, PowerSupply> POWERSUPPLY_MAP;
 
-	private static LinkedList<PowerSupply> POWERSUPPLY_LIST;
+  public PowerData(HashMap<String, PowerSupply> powerList) {
+    POWERSUPPLY_MAP = powerList;
+  }
 
+  public HashMap<String, PowerSupply> getPowerList() {
+    return POWERSUPPLY_MAP;
+  }
 
+  /**
+   * Returns the PowerSupply with the given ID.
+   * 
+   * @param id
+   *          ID of the PowerSupply
+   * @return PowerSupply with the ID, null if no PowerSupply was found
+   */
+  public static PowerSupply getById(String id) {
 
-	public PowerData(){
-		POWERSUPPLY_LIST = new LinkedList<PowerSupply>();
-		
-	}
-	
-	public PowerData(LinkedList<PowerSupply> powerList){
-		POWERSUPPLY_LIST = powerList;
-	}
+    PowerSupply ps;
 
-	public LinkedList<PowerSupply> getPowerList() {
-	    return POWERSUPPLY_LIST;
-	  }
+    ps = POWERSUPPLY_MAP.get(id);
 
-	  
-	  /**
-	   * Returns the PowerSupply with the given ID.
-	   * @param id ID of the PowerSupply
-	   * @return PowerSupply with the ID, null if no PowerSupply was found
-	   */
-	  static public PowerSupply getById(String id){
-	    
-		  PowerSupply ps = null;
-	    for (PowerSupply powerSupply : POWERSUPPLY_LIST) {
-	        if(powerSupply.getId().equals(id)){
-	            ps = powerSupply;
-	        }
-	    }
-	    
-	   
-	    return ps;
-	  }
+    return ps;
+  }
 
-	  /**
-	   * Adds a PowerSupply to the global data.
-	   * @param ps PowerSupply to add
-	   */
-	  static public void addPs(PowerSupply ps){
-	    POWERSUPPLY_LIST.add(ps);
-	  }
-	  
-	  /**
-	   * Removes a PowerSupply from the global data.
-	   * @param ps PowerSupply to remove
-	   * @return bool true if PowerSupply was in the list and was removed, false if PowerSupply was not in the list
-	   */
-	  public static boolean removePs(PowerSupply ps){
-	    
-		  boolean bool = false;
-		  for (PowerSupply powerSupply : POWERSUPPLY_LIST) {
-	        if(ps == powerSupply){
-	            POWERSUPPLY_LIST.remove(powerSupply);
-	            bool = true;
-	        }
-	    }
-		  return bool;
-	  }
+  /**
+   * Adds a PowerSupply to the global data.
+   * 
+   * @param ps
+   *          PowerSupply to add
+   */
+  public static void addPs(PowerSupply ps) {
+    POWERSUPPLY_MAP.put(ps.getId(), ps);
+  }
 
-	  
-	  /**
-	   * Removes a PowerSupply from the global data
-	   * @param id ID of the PowerSupply to remove
-	   * @return bool true if PowerSupply was in the list and was removed, false if PowerSupply was not in the list
-	   */
-	  public static boolean removePs(String id){
-		  boolean bool = false;
-		  
-		  for (PowerSupply ps : POWERSUPPLY_LIST) {
-			if(ps.getId().equals(id)){
-				POWERSUPPLY_LIST.remove(ps);
-				bool = true;
-			}
-		}
-		  
-		  return bool;
-	  }
-	  
-	  
-	  
-	  /**
-	   * Returns if a PowerSupply with a given ID exists in the global data
-	   * @param id PowerSupply ID to look for
-	   * @return false if the PowerSupply does not exist, true if it does
-	   */
-	  public static boolean exists(String id){
-		 
-		 boolean bool = false;
-		 
-		 for (PowerSupply ps : POWERSUPPLY_LIST) {
-			if(ps.getId().equals(id)){
-				bool = true;
-			}
-		}
-		 return bool;
-	  }
+  /**
+   * Removes a PowerSupply from the global data.
+   * 
+   * @param ps
+   *          PowerSupply to remove
+   * @return boolean true if PowerSupply was in the list and was removed, false if
+   *         PowerSupply was not in the list
+   */
+  public static boolean removePs(PowerSupply ps) {
+
+    String id = ps.getId();
+
+    PowerSupply ps1 = POWERSUPPLY_MAP.remove(id);
+
+    if (ps1 == null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  /** Removes a PowerSupply from the global data.
+   * 
+   * @param id
+   *          ID of the PowerSupply to remove
+   * @return bool true if PowerSupply was in the list and was removed, false if
+   *         PowerSupply was not in the list
+   */
+  public static boolean removePs(String id) {
+
+    PowerSupply ps = POWERSUPPLY_MAP.remove(id);
+
+    if (ps == null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  /** Returns if a PowerSupply with a given ID exists in the global data.
+   * 
+   * @param id
+   *          PowerSupply ID to look for
+   * @return false if the PowerSupply does not exist, true if it does
+   */
+  public static boolean exists(String id) {
+
+    if (POWERSUPPLY_MAP.get(id) != null) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
 }
