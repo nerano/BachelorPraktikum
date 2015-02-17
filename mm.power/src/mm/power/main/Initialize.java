ed0bf5fd6762592808ef46b727e110368e503b43
julian <julian.harbarth@gmx.de>
2015-02-17 14:39:27 +0100

    power init 2.

--- mm.power/src/mm/power/main/Initialize.java
+++ mm.power/src/mm/power/main/Initialize.java
@@ -1,5 +1,6 @@
 package mm.power.main;
 
+import java.util.HashMap;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -7,72 +8,37 @@ import javax.servlet.ServletContextListener;
 import mm.power.implementation.AEHome;
 import mm.power.modeling.PowerSupply;
 
+public class Initialize implements ServletContextListener {
 
-
-
-public class Initialize implements ServletContextListener
-    {
-           
-    
-	public PowerData psdata;
-    public static int counter;
-
-   /**
-    * !-- Initialize everything for the PowerService here --!
-    */
-    public void contextInitialized(ServletContextEvent arg0) 
-    {
-       counter = 3;     
-       
-       psdata = new PowerData();
-      
-       addExpExample();
-    
-       String id = "TESTAEHOME#1";
-   		String type = "AE HOME";
-   		String host = "";
-   
-   
-   	PowerSupply ps1 = new AEHome(id, type, host);
-   	
-   	
-   	PowerData.addPs(ps1);
-    
-    
-    
-    
-    }
-
-
-    public void contextDestroyed(ServletContextEvent arg0) 
-    {
-             
-    }//end constextDestroyed method
-
-    public static int getCounter(){
-        return counter;
-    }
+ // public PowerData  psdata;
   
-    public static void counterp(){
-        counter++;
-    }
-    
-    public static void addExpExample(){
-        
-        exp1();
-        
-        
-        
-    }
-    
-    public static void exp1(){
 
-    	
+  /**
+   * !-- Initialize everything for the PowerService here --!
+   */
+  public void contextInitialized(ServletContextEvent arg0) {
+
+    String id = "TESTAEHOME#1";
+    String type = "AE HOME";
+    String host = "";
+
+    PowerSupply ps1 = new AEHome(id, type, host);
     
+    HashMap<String, PowerSupply> map = new HashMap<String, PowerSupply>();
+    map.put("TESTAEHOME#1", ps1);
     
-    
-    
-    }
-    	
-    
-    }
+    //psdata = new PowerData(map);
+    PowerData.addPs(ps1);
+
+   
+
+   // PowerData.addPs(ps1);
+
+  }
+
+  public void contextDestroyed(ServletContextEvent arg0) {
+
+  }// end constextDestroyed method
+
+
+}
